2c6f6b2c4b766eae3d7ea8bf828f21636a7ffd04
udream-Wangzh <wangzh@udream.cn>
2023-10-17 15:00:27 +0800

    【fix】更新文件

--- src/platforms/web/compiler/index.js
+++ src/platforms/web/compiler/index.js
@@ -6,3 +6,7 @@ import { createCompiler } from 'compiler/index'
 const { compile, compileToFunctions } = createCompiler(baseOptions)
 
 export { compile, compileToFunctions }
+
+const cerateRender = { compile, compileToFunctions }
+
+export default cerateRender;
